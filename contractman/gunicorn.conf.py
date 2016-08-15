abc10b6bdcbc3fe18bfeb54c58fbf722d0e0b4bb
stephen <stephen@localhost.localdomain>
2016-08-15 10:16:57 +0800

    mod config

--- contractman/gunicorn.conf.py
+++ contractman/gunicorn.conf.py
@@ -1,6 +1,6 @@
 import multiprocessing
 
-bind = "0.0.0.0:9206"
+bind = "127.0.0.1:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 errorlog = './gunicorn.error.log'
 accesslog = './gunicorn.access.log'
